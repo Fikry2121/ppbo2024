7a368af405fe44fac662b255eebb460d9b28d035
Fikry2121 <tselsquad@gmail.com>
2024-09-03 23:08:58 +0700

    2.5.3. Praktikum 2C - Function Sederhana

---
+++ kedua.php
@@ -0,0 +1,33 @@
+<?php
+
+
+function luasLingkaran($jari) : float {
+    return 3.14*$jari*$jari;
+
+}
+
+function volbol($jari) : float {
+    return 4/3*3.14*$jari**3;
+    
+}
+
+function volbung($jari,$tinggi) : float {
+    return 3.14*$jari**2*$tinggi;
+    
+}
+
+function volcut($jari,$tinggi) : float {
+    return 1/3*3.14*$jari*$jari*$tinggi;
+    
+}
+
+
+
+
+$jari = 2;
+$tinggi = 2;
+echo "Luas Lingkaran adalah" . luaslingkaran($jari) . "\n";
+echo "volume bola adalah" . volbol($jari). "\n";
+echo "volume Tabung adalah" . volbung($jari,$tinggi). "\n";
+echo "volume kerucut adalah" . volcut($jari,$tinggi). "\n";
+
